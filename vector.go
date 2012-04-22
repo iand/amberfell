8245a1164902ee735a3056c82266054f9abd343e
Ian Davis <me@iandavis.com>
2012-04-22 14:36:07 +0100

    General code tidying up

--- vector.go
+++
@@ -1,6 +0,0 @@
-/*
-  To the extent possible under law, Ian Davis has waived all copyright
-  and related or neighboring rights to this Amberfell Source Code file.
-  This work is published from the United Kingdom. 
-*/
-package main
